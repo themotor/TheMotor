e3cd875ef6f5a5f77748ceb73090227e464b751a
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2019-07-14 23:32:03 +0200

    Add window integration to engine

--- motor/engine.h
+++ motor/engine.h
@@ -1,10 +1,19 @@
 #ifndef _MOTOR_ENGINE_H_
 #define _MOTOR_ENGINE_H_
 
+#include <memory>
+
+#include "window.h"
+
 namespace motor
 {
 class Engine
 {
+ public:
+  void InitializeWindow(WindowOptions opts);
+
+ private:
+  std::unique_ptr<Window> window_manager_;
 };
 }  // namespace motor
 
