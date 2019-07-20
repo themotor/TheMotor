81378f31670abe10ae541863d10e0af9e836ea3a
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2019-07-20 17:44:37 +0200

    Handle key callbacks in glfw

--- motor/windows/glfw_window.cpp
+++ motor/windows/glfw_window.cpp
@@ -31,6 +31,7 @@ class GLFWWindow : public Window
 
     glfwMakeContextCurrent(handle_);
     glfwSetWindowCloseCallback(handle_, CloseCallback);
+    glfwSetKeyCallback(handle_, KeyCallback);
   }
 
   ~GLFWWindow() final
@@ -41,13 +42,14 @@ class GLFWWindow : public Window
 
   void Update() override
   {
+    glfwPollEvents();
+    ProcessInputs();
     // TODO(kadircet): Move this into renderer, once we have one ...
     // Just to check it works, should generate a blue window.
     glClearColor(0., 0., 1., 1.);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(handle_);
-    glfwPollEvents();
   }
 
  private:
@@ -55,6 +57,16 @@ class GLFWWindow : public Window
   // Unfortunately we can't use a unique_ptr here, because glfw headers only
   // forward declares GLFWwindow.
   GLFWwindow* handle_;
+  std::vector<input::KeyInput> key_inputs_;
+
+  void ProcessInputs()
+  {
+    input::InputStateBroadcast broadcast;
+    broadcast.keys_ = std::move(key_inputs_);
+    // TODO(kadircet): Poll non-event based devices.
+
+    GetDispatcher().Dispatch(broadcast);
+  }
 
   static void CloseCallback(GLFWwindow* window)
   {
@@ -63,10 +75,24 @@ class GLFWWindow : public Window
         static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
     glfw_window->GetDispatcher().Dispatch(WindowClose());
   }
+
+  static void KeyCallback(GLFWwindow* window, int key, int scancode, int action,
+                          int mods)
+  {
+    VLOG(1) << "Received key callback: " << key << ' ' << scancode << ' ' << ' '
+            << action << ' ' << mods << " - " << glfwGetKeyName(key, scancode);
+    if (key == GLFW_KEY_UNKNOWN) return;
+
+    auto* glfw_window =
+        static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+    glfw_window->key_inputs_.push_back(
+        {input::KEYBOARD_0, key, action != GLFW_RELEASE});
+  }
 };
 
 }  // namespace
 
+// Sets GLFWWindow as the window manager when linked in.
 static WindowPlugin::Set<GLFWWindow> x;
 
 }  // namespace motor
