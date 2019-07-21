47e2d58b3e001dff831cc38149acf3eb7e8d9360
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2019-07-21 14:26:51 +0200

    Command buffers and pool

--- motor/render/vulkan_renderer.cpp
+++ motor/render/vulkan_renderer.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <tuple>
 #include <vector>
 
@@ -58,12 +59,12 @@ vk::PhysicalDevice SelectPhyiscalDevice(const vk::Instance& vk_inst)
   return devices.front();
 }
 
-int GetGraphicsQueueIdx(const vk::PhysicalDevice& phy_dev)
+size_t GetGraphicsQueueIdx(const vk::PhysicalDevice& phy_dev)
 {
   std::vector<vk::QueueFamilyProperties> queue_family_props =
       phy_dev.getQueueFamilyProperties();
   CHECK(!queue_family_props.empty()) << "No queues on the device";
-  int result = -1;
+  size_t result = -1;
   for (int i = 0; i < queue_family_props.size(); ++i)
   {
     const auto& family_prop = queue_family_props[i];
@@ -75,16 +76,15 @@ int GetGraphicsQueueIdx(const vk::PhysicalDevice& phy_dev)
   return result;
 }
 
-vk::Device CreateDevice(const vk::Instance& vk_inst)
+vk::Device CreateDevice(const vk::PhysicalDevice& phy_dev,
+                        size_t queue_family_idx)
 {
-  vk::PhysicalDevice phy_dev = SelectPhyiscalDevice(vk_inst);
-
   vk::DeviceQueueCreateInfo queue_create_info;
   // Currently we support only a single queue, therefore this is not important.
   const float queue_prios = {.0};
   queue_create_info.setPQueuePriorities(&queue_prios)
       .setQueueCount(1)
-      .setQueueFamilyIndex(GetGraphicsQueueIdx(phy_dev))
+      .setQueueFamilyIndex(queue_family_idx)
       .setFlags(static_cast<vk::DeviceQueueCreateFlags>(0))
       .setPNext(nullptr);
 
@@ -103,18 +103,49 @@ vk::Device CreateDevice(const vk::Instance& vk_inst)
                          "Couldn't create logical device");
 }
 
+vk::CommandPool CreateCommandPool(const vk::Device& vk_dev,
+                                  size_t queue_family_idx)
+{
+  vk::CommandPoolCreateInfo pool_create_info;
+  pool_create_info.setFlags(static_cast<vk::CommandPoolCreateFlags>(0))
+      .setQueueFamilyIndex(queue_family_idx)
+      .setPNext(nullptr);
+  return VkSuccuessOrDie(vk_dev.createCommandPool(pool_create_info),
+                         "Couldn't create command pool");
+}
+
+std::vector<vk::CommandBuffer> AllocateCommandBuffers(
+    const vk::Device& vk_dev, const vk::CommandPool& cmd_pool, size_t count)
+{
+  vk::CommandBufferAllocateInfo cmd_bufer_alloc_info;
+  cmd_bufer_alloc_info.setCommandBufferCount(count)
+      .setCommandPool(cmd_pool)
+      .setLevel(vk::CommandBufferLevel::ePrimary)
+      .setPNext(nullptr);
+  return VkSuccuessOrDie(vk_dev.allocateCommandBuffers(cmd_bufer_alloc_info),
+                         "Couldn't allocate command buffers");
+}
+
 class VulkanRenderer : public Renderer
 {
  public:
   VulkanRenderer()
   {
     vk_instance_ = CreateInstance();
-    vk_device_ = CreateDevice(vk_instance_);
+    phy_dev_ = SelectPhyiscalDevice(vk_instance_);
+    queue_family_idx_ = GetGraphicsQueueIdx(phy_dev_);
+    vk_device_ = CreateDevice(phy_dev_, queue_family_idx_);
+    vk_cmd_pool_ = CreateCommandPool(vk_device_, queue_family_idx_);
+
+    auto buffers = AllocateCommandBuffers(vk_device_, vk_cmd_pool_, 1);
   }
   void Render() override {}
 
  private:
+  vk::CommandPool vk_cmd_pool_;
   vk::Device vk_device_;
+  size_t queue_family_idx_;
+  vk::PhysicalDevice phy_dev_;
   vk::Instance vk_instance_;
 };
 
